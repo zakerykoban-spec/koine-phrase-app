577e3c8fbd594614804410d3c088093accf0e361
zakerykoban-spec <zakery.koban@gmail.com>
2026-08-12 23:16:49 -0400

    fix: load iPhone safe area styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './interaction.css'
+import './safe-area.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
